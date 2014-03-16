0099af613f1ba87fab46373705d56142814e5547
jlewin <lewin76@gmail.com>
2014-03-16 15:11:24 -0700

    Fixed issue with binary stls  - Added simple array to support alternate library paths

--- editor/Loader.aspx.cs
+++ editor/Loader.aspx.cs
@@ -8,14 +8,20 @@ using System.Web.UI.WebControls;
 
 public partial class editor_Loader : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
-    {
-		string basePath = @"E:\src-ext\PrusaMendel\metric-prusa";
-		string path = Path.Combine(basePath, Request["stlFile"]);
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		// Silly short term mechanism for repo selection
+		var repos = new []{
+		@"E:\src-ext\PrusaMendel\metric-prusa",
+		@"E:\Redirected\Users\john\Downloads"};
+
+		string path = Path.Combine(repos[1], Request["stlFile"]);
+
+		Response.ContentType = "application/octet-stream";
 
 		if (File.Exists(path) && Path.GetExtension(path).ToLower() == ".stl")
 		{
-			Response.Write(File.ReadAllText(path));
+			Response.WriteFile(path);
 		}
-    }
+	}
 }
